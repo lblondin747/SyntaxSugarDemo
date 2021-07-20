5ca2aec8c78ec1502c3f4e0944aa39a79a3a0217
Luke Blondin <lblondin747@gmail.com>
2021-07-20 11:50:01 -0500

    initial commit

---
+++ SyntaxSugarDemo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SyntaxSugarDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           var answer = 12;
+           Console.WriteLine((answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater than or equal to nine");
+        }
+    }
+}
